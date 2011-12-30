ed379cc1f4ed241102d31196476c46cfcfc17fa2
Adam Vaughan <adamjvaughan@gmail.com>
2011-12-30 12:24:46 -0700

    add support for deploying to a subdirectory in production

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,8 @@
+
+if ENV['RAILS_ENV'] == 'production'
+  ENV['RAILS_RELATIVE_URL_ROOT'] = '/gpx'
+end
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Gpx::Application.routes.draw do
-  resources :segments, :only => [:index, :update, :destroy] do
-    resources :points, :only => :index
+  scope ENV['RAILS_RELATIVE_URL_ROOT'] || '' do
+    resources :segments, :only => [:index, :update, :destroy] do
+      resources :points, :only => :index
+    end
+
+    resources :uploads, :only => :create
+    match '/reports/totals' => 'reports#totals'
+    match '/reports/monthly' => 'reports#monthly'
+    match '/records' => 'records#show'
+
+    root :to => 'segments#index'
   end
-
-  resources :uploads, :only => :create
-  match '/reports/totals' => 'reports#totals'
-  match '/reports/monthly' => 'reports#monthly'
-  match '/records' => 'records#show'
-
-  root :to => 'segments#index'
 end
