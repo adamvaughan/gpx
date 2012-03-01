904da45b9b63542c32419d792318f5837b1d1f40
Adam Vaughan <adamjvaughan@gmail.com>
2012-02-29 21:54:08 -0700

    get rid of routes that aren't currently being used

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Gpx::Application.routes.draw do
   scope ENV['RAILS_RELATIVE_URL_ROOT'] || '' do
-    resources :segments, :path => 'rides', :only => [:index, :show, :update, :destroy] do
-      resources :points, :only => :index
-    end
+    resources :segments, :path => 'rides', :only => [:index, :show]
 
     root :to => 'summary#index'
   end
