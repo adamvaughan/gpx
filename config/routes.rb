60c4c1069fc9c8d64ea9cac83eeac250e8023d18
Adam Vaughan <adamjvaughan@gmail.com>
2012-02-29 21:42:03 -0700

    add routes for segments and points

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Gpx::Application.routes.draw do
   scope ENV['RAILS_RELATIVE_URL_ROOT'] || '' do
-
+    resources :segments, :path => 'rides', :only => [:index, :show, :update, :destroy] do
+      resources :points, :only => :index
+    end
 
     root :to => 'summary#index'
   end
